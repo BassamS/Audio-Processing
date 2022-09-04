f1b00fd3eb2e050ba6919afbaefbeed80832ffca
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-04 10:00:40 +0200

    Creating a new WAV file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import wave
 
+# Open audio in READ binary mode!
 obj = wave.open("sample.wav", "rb")
 
 print("Number of channels", obj.getnchannels())
@@ -9,8 +10,21 @@ print("Number of Frame", obj.getnframes())
 print("Parameters", obj.getparams())
 
 time_audio = obj.getframerate() / obj.getframerate()
-print("Time", time_audio)
+print(time_audio)
 
 frames = obj.readframes(-1)
 print(type(frames), type(frames[0]))
-print(len(frames))
+print(len(frames) / 2)
+
+obj.close()
+
+# Open audio in WRITE binary mode!
+obj_new = wave.open("new_sample.wav", "wb")
+
+obj_new.setnchannels(1)
+obj_new.setsampwidth(2)
+obj_new.setframerate(8000.0)
+
+obj_new.writeframes(frames)
+
+obj_new.close()
