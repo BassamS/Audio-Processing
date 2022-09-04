824b0fada460df7f985504716365ae83482c6abc
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-04 10:58:12 +0200

    freq GPU

--- plot_audio.py
+++ plot_audio.py
@@ -10,3 +10,20 @@ num_samples = obj.getnframes()
 signal_wave = obj.readframes(-1)
 
 obj.close()
+
+# Length of audio in seconds
+t_audio = num_samples / sample_freq
+
+print(t_audio)
+
+signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+
+times = np.linspace(0, t_audio, num=num_samples)
+
+plt.figure(figsize=(15, 5))
+plt.plot(times, signal_array)
+plt.title("Audio Signal")
+plt.ylabel("Signal Wave")
+plt.xlabel("Time (s)")
+plt.xlim(0, t_audio)
+plt.show()
