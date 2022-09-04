cdbcda201bfaa65a593a1fb9e34640d7d509d415
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-04 09:30:13 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import wave
 
-obj = wave.open("sample.wave", "rb")
+obj = wave.open("sample.wav", "rb")
 
 print("Number of channels", obj.getnchannels())
 print("Sample width", obj.getsampwidth())
 print("Frame rate", obj.getframerate())
 print("Number of Frame", obj.getnframes())
+print("Parameters", obj.getparams())
+
+time_audio = obj.getframerate() / obj.getframerate()
+print("Time", time_audio)
+
+frames = obj.readframes(-1)
+print(type(frames), type(frames[0]))
+print(len(frames))
